d437318b44f14207489c7e49e5d518b27b5dbc17
Fanttatraieste <118627923+Fanttatraieste@users.noreply.github.com>
2023-07-05 16:16:36 +0300

    popcorn

--- 07-usepopcorn/starter/usepopcorn/src/index.js
+++ 07-usepopcorn/starter/usepopcorn/src/index.js
@@ -8,6 +8,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["terrible", "bad", "ok", "good", "amazing"]}
+    />
+    <StarRating maxRating={10} color={"red"} defaultRating={3} />
   </React.StrictMode>
 );
